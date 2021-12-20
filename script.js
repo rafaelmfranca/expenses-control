7c568523b39a7f5fdc12162aa6ceb7af61611262
Rafael França <rafaelmonfardinifranca@gmail.com>
2021-12-20 15:38:26 -0300

    :sparkles: Added update current balance feature

--- script.js
+++ script.js
@@ -14,13 +14,20 @@ const addTransactionIntoDOM = ({ name, amount }) => {
   const li = document.createElement('li');
 
   li.classList.add(liClass);
-  li.innerHTML = `<small>${name}</small> <span class="${spanClass}">$ ${Math.abs(amount)}</span>`;
+  li.innerHTML = `<small>${name}</small> <span class="${spanClass}">$ ${Math.abs(amount).toFixed(2)}</span>`;
 
   transactionsUl.appendChild(li);
 };
 
+const updateCurrentBalance = () => {
+  const currentBalance = get('#balance');
+  const total = transactions.reduce((acc, { amount }) => acc + amount, 0).toFixed(2);
+  currentBalance.textContent = `$ ${total}`;
+};
+
 const init = () => {
   transactions.forEach(addTransactionIntoDOM);
+  updateCurrentBalance();
 };
 
 init();
